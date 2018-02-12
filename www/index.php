15a7ef34fec2e5ee911055853271a32870b51033
gregorwebmaster <git@gregorwebmaster.com>
2018-02-12 06:01:22 +0100

    update Dockerfile & config

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+</head>
+<body>
+<?php
+echo '<h1>Hello world</h1>';
+echo '<p>Your php works correctly</p>';
+?>
+</body>
+</html>
